f0983e35034eaf26c9dd808113f4b177001af8fd
Diogo SIlva <272430@unifio.edu.br>
2026-09-01 00:22:48 +0000

    new acao

--- ong-tarefas-grupo404/src/app/app-routing-module.ts
+++ ong-tarefas-grupo404/src/app/app-routing-module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoModule } from './produto/produto-module';
 
 const routes: Routes = [
   {
     path: 'categoria', 
     loadChildren: () => import('./categoria/categoria-module').then(m => m.CategoriaModule)
   },
+  {
+    path: 'prooduto',
+    loadChildren: () => import('./produto/produto-module').then(m => m.ProdutoModule)
+  }
 ];
 
 @NgModule({
